4d33126d44fa301bff61c2f847b4f652ed41c3f5
Yash Bhatnagar <58135394+superzackx@users.noreply.github.com>
2022-01-23 12:32:08 +0530

    Update installer.sh

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,8 +1,13 @@
-git clone https://github.com/superzackx/quickmodel.git
+git clone --quiet https://github.com/superzackx/quickmodel.git
 cd quickmodel
 pip install -q -r requirements.txt 
 cd src
 sudo chmod +x quickmodel.py
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-printf "You have successful installed ${RED}QuickModel by SuperZackX.${NC}\n"
+cat << "EOF"
+  ___        _      _    __  __           _      _ 
+ / _ \ _   _(_) ___| | _|  \/  | ___   __| | ___| |
+| | | | | | | |/ __| |/ / |\/| |/ _ \ / _` |/ _ \ |
+| |_| | |_| | | (__|   <| |  | | (_) | (_| |  __/ |
+ \__\_\\__,_|_|\___|_|\_\_|  |_|\___/ \__,_|\___|_|
+
+EOF
