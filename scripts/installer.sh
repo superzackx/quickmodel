18b09e59aa1cc66a78fb197a588f6e15ad4c72ce
Yash Bhatnagar <58135394+superzackx@users.noreply.github.com>
2022-02-03 14:57:41 +0530

    Update installer.sh

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,6 +1,6 @@
-git clone --quiet https://github.com/superzackx/quickmodel.git
+git clone https://github.com/superzackx/quickmodel.git
 cd quickmodel
-pip install -q -r requirements.txt 
+pip install -r requirements.txt 
 cd src
 sudo chmod +x quickmodel.py
 cat << "EOF"
